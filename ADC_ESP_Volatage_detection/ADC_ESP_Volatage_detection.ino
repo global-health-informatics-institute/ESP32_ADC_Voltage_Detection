402099596d57d3e6e42f522d7f1b6ff3b284b111
Duntar <mwabilungu@gmail.com>
2020-06-22 09:40:34 +0200

    Add files via upload

--- ADC_ESP_Volatage_detection/ADC_ESP_Volatage_detection.ino
+++ ADC_ESP_Volatage_detection/ADC_ESP_Volatage_detection.ino
@@ -12,16 +12,17 @@ int get_max() {
   int ADC_VALUE = 0; 
   
   Vmax = 0;
-  for(int i = 0; i < 40; i++) {
+  for(int i = 0; i < 80; i++) {
      ADC_VALUE = analogRead(Analog_channel_pin);  // read from analog channel 3 (A3)
     if(ADC_VALUE > Vmax) Vmax = ADC_VALUE;
+    delayMicroseconds(250);
   }  
   return Vmax;
 }
 
 void loop() {
   
-  Serial.println("ADC Value:" + String(get_max()*3.3*0.7071/4096));
+  Serial.println("ADC Value:" + String(get_max()*407*0.7071/4096));
   delay(1000);
 
   
