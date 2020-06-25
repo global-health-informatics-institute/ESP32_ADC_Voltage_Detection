d49b1c0a5b5d69c7b441ed4586130ddbad262843
Duntar <mwabilungu@gmail.com>
2020-06-25 10:58:54 +0200

    Add files via upload

---
+++ adcSimple/adcSimple.ino
@@ -0,0 +1,43 @@
+/*
+  Example for MCP3002 - Library for communicating with MCP3002 Analog to digital converter.
+  Based on the MCP3008 Library created by Uros Petrevski, Nodesign.net 2013
+  Released into the public domain.
+*/
+
+#include <MCP3002.h>
+
+
+// define pin connections
+#define CS_PIN 12
+#define CLOCK_PIN 9
+#define MOSI_PIN 11
+#define MISO_PIN 10
+
+// put pins inside MCP3002 constructor
+MCP3002 adc(CLOCK_PIN, MOSI_PIN, MISO_PIN, CS_PIN);
+
+void setup() {
+ 
+ // open serial port
+ Serial.begin(9600);
+  
+}
+
+
+void loop() {
+  
+  
+  int val = adc.readADC(0); // read Chanel 0 from MCP3008 ADC
+  Serial.print("ADC Value ");
+  Serial.println(val);
+  float volts = val* ( 3.3 / 1023.0 );
+  Serial.print("Volts ");
+  Serial.println(volts);
+  delay(1000);
+/*  val = adc.readADC(1); // read Chanel 0 from MCP3008 ADC
+    Serial.println(val);
+    delay(1000);
+    */
+  
+  
+}
